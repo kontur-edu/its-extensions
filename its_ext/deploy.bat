6b15efe390d1ab11c335085b48b1fcb309d782c2
KachusovStepan <stepanurfu@gmail.com>
2022-07-15 08:29:57 +0500

    fixed deploy.bat and added instructions to README.md

--- its_ext/deploy.bat
+++ its_ext/deploy.bat
@@ -1,5 +1,4 @@
 rmdir /S /Q build 2>nul
 npm run build
-@REM Нужно проверить доступ для просмотра списка объектов (кажется, viewer не хватили)
-aws --endpoint-url=https://storage.yandexcloud.net s3 rm s3://its-extensions/test/ --recursive
-aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive build/ s3://its-extensions/test/
+aws --endpoint-url=https://storage.yandexcloud.net s3 rm s3://its-extensions/ --recursive
+aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive build/ s3://its-extensions/

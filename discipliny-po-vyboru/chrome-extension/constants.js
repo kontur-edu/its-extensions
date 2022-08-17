daa376971e47c1e0586eba6c5ddbf035e5ef39ea
Stepan Kachusov <stepanurfu@gmail.com>
2022-08-17 16:38:41 +0500

    added notion function proxy to deploy-scripts

--- discipliny-po-vyboru/chrome-extension/constants.js
+++ discipliny-po-vyboru/chrome-extension/constants.js
@@ -1,8 +1,7 @@
 const PROXY_URL_KEY = "its_proxy_url";
 const NOTION_MAIN_PAGE_KEY = "its_notion_main_page_url";
 
-const PROXY_URL_VALUE =
-  "https://d5dfhr6m42a5gefn5qmb.apigw.yandexcloud.net/notion/";
+const PROXY_URL_VALUE = "https://<Доменное имя API-Gateway>/notion/";
 const NOTION_MAIN_PAGE_VALUE =
   "https://fiiturfu.notion.site/423725f4115046c9bc29df894a87dbe1?v=5051b6fbb5d34ad38aa681202d595071";
 
